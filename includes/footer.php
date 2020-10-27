69f094dcefe77183c214f9f2d3376b19770ee718
Ilan Granot <ilangranot@gmail.com>
2020-10-26 18:11:57 -0700

    built app tree

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+    <footer class="page-footer dark">
+        <div class="footer-copyright">
+            <p>© 2020 The Cool Guys</p>
+        </div>
+    </footer>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html style="height:100%">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Home - Santa Clara Menus</title>
+    <link rel="stylesheet" href="assets/css/scm.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+
+<body style="height:100%;min-height:100%">
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#">Santa Clara Menus</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">HOME</a></li>
+                    <?php
+                    if ($_SESSION['authenticated'] == true){
+                       echo '<li class="nav-item"><a class="nav-link" href="app.php">APP</a></li>
+                            <li class="nav-item"><a class="nav-link" href="logout.php">LOGOUT</a></li>';
+                    } else {
+                    echo '<li class="nav-item"><a class="nav-link" href="signup.php">SIGN UP</a></li>
+                          <li class="nav-item"><a class="nav-link" href="login.php">LOGIN</a></li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>

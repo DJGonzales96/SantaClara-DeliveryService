990b8a2e67315822717fcbebb4ead5aad45e6e8c
Devin Gonzales <devin.j.gonzales@gmail.com>
2020-10-29 16:41:29 -0700

    updated DB schema, username "bug" fix

--- OTHERS/database_init.sql
+++ OTHERS/database_init.sql
@@ -1,4 +1,4 @@
---- INITIALIZATION ---
+/* INITIALIZATION */
 DROP DATABASE IF EXISTS santa_clara_menus;
 CREATE DATABASE santa_clara_menus;
 USE santa_clara_menus;
@@ -9,46 +9,37 @@ GRANT ALL PRIVILEGES ON santa_clara_menus . * TO 'scmuser'@'localhost';
 FLUSH PRIVILEGES;
 */
 
---- ENTITIES ---
+/* ENTITIES */
 CREATE TABLE IF NOT EXISTS user (
 	user_id	INT(3) UNSIGNED AUTO_INCREMENT,
     username VARCHAR(64) UNIQUE,
     name VARCHAR(64),
     encrypted_password VARCHAR(64) NOT NULL,
     isRestaurant BOOLEAN,
+    loc INT(3) UNSIGNED,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS location (
-	user_id INT(3) UNSIGNED,
+	loc_id INT(3) UNSIGNED AUTO_INCREMENT,
+    latitude VARCHAR(64),
+    longitude VARCHAR(64),
     address VARCHAR(64),
-	PRIMARY KEY (user_id)
+	PRIMARY KEY (loc_id)
 );
 
 CREATE TABLE IF NOT EXISTS transaction (
 	t_id INT(3) UNSIGNED AUTO_INCREMENT,
 	driver_id INT(3) UNSIGNED,
     restaurant_id INT(3) UNSIGNED,
-    start_loc VARCHAR(64),
-    end_loc VARCHAR(64),
+    start_loc INT(3) UNSIGNED,
+    end_loc INT(3) UNSIGNED,
     timestamp TIMESTAMP,
-    duration BIGINT, 
+    duration BIGINT,
     price DECIMAL(5,2),
+    active BOOLEAN,
     PRIMARY KEY (t_id)
 );
 
---- DATA ---
-INSERT INTO user (username, name, encrypted_password, isRestaurant) VALUES
-	('testDriver1', 'driver1Name', '$1$O3JMY.Tw$AdLnLjQ/5jXF9.MTp3gHv/', FALSE),
-    ('testDriver2', 'driver2Name', '$1$O3JMY.Tw$AdLnLjQ/5jXF9.MTp3gHv/', FALSE),
-    ('testRest1', 'rest1Name', '$1$O3JMY.Tw$AdLnLjQ/5jXF9.MTp3gHv/', TRUE),
-	('testRest2', 'rest2Name', '$1$O3JMY.Tw$AdLnLjQ/5jXF9.MTp3gHv/', TRUE);
 
-INSERT INTO location (user_id, address) VALUES
-	(001, '1234 Test Start Address Rd'),
-    (002, '1234 Test Start Address Rd'),
-    (003, '1234 Test Restaurant Address Rd'),
-    (004, '1234 Test Restaurant Address Rd');
-
-INSERT INTO transaction (driver_id, restaurant_id, start_loc, end_loc, timestamp, duration, price) VALUES
-	(001, 003, '1234 Test Restaurant Address Rd', '4321 Customer Addr Rd', '1970-01-01 00:00:01', 3600, 24.99);
+/* TODO: NEW TEST DATA */

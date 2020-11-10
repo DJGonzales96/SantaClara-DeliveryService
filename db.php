16911ce0b872a28578ed36dfb2ff9b06ae6f6abf
Ilan Granot <ilangranot@gmail.com>
2020-11-10 13:15:30 -0800

    progress on front and back ends

---
+++ db.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'includes/conn.php';
+require_once 'includes/session.php';
+require_once 'includes/auth.php';
+
+// prevent unauthorized access
+if ($_SESSION['authenticated'] != true || $_SESSION["username"] == NULL)
+    die("{'status':'STATUS_ERROR','error':'Not logged in'}");
+
+function dbQueryError($conn, $where){
+    $dumpStr = "in db query, dump: " .  $conn->error . ", where: " . $where;
+    die("{'status':'STATUS_ERROR','error':'". $dumpStr . "'}");
+}
+
+// Inserts a new location
+function insertNewLocationToDb(String $newLat, String $newLong, String $newAddr) {
+    global $conn;
+    $query = "INSERT INTO location(lat, lon, address) VALUES('$newLat', '$newLong', '$newAddr')";
+    $result = $conn->query($query);
+    if(!$result)
+        dbQueryError($conn, "insertNewLocationToDb");
+    return $conn->insert_id; // the location id is the most recently inserted
+}
+
+// Inserts a new transaction
+function insertNewTransactionToDb($user_id, $start_location, $t_type){
+    global $conn;
+    // echo "t_type: " . $t_type . ", user_id: ". $user_id . ", start_location: ". $start_location; // REMOVE DEBUG  REMOVE DEBUG  REMOVE DEBUG  REMOVE DEBUG  REMOVE DEBUG  REMOVE DEBUG  REMOVE DEBUG
+    $query = "INSERT INTO transaction(t_type, restaurant_id, start_loc) VALUES('$t_type', '$user_id', '$start_location')";
+    $result = $conn->query($query);
+    if(!$result)
+        dbQueryError($conn, "insertNewTransactionToDb");
+    return $conn->insert_id;
+}
+
+function updateUserTransactionInDb($user_id,$new_t_id){
+    global $conn;
+    $query = "UPDATE User SET t_id=' $new_t_id' WHERE user_id='$user_id'";
+    $result = $conn->query($query);
+    if(!$result)
+        dbQueryError($conn, "updateUserTransactionInDb");
+}
+
+function getLocationIdByTid($t_id){
+    global $conn;
+    $query = "SELECT start_loc FROM transaction WHERE t_id = '$t_id'";
+    $result = $conn->query($query);
+    if(!$result)
+        dbQueryError($conn, "getLocationByTid");
+    $row = $result->fetch_array(MYSQLI_NUM);
+    $loc = $row[0];
+    return $loc;
+}
+
+function getLocationByLocId(String $loc_id)
+{
+    global $conn;
+    $query = "SELECT * FROM location WHERE loc_id = '$loc_id'";
+    $result = $conn->query($query);
+    if(!$result)
+        dbQueryError($conn, "getLocationByLocId");
+
+    $row = $result->fetch_array(MYSQLI_NUM);
+    $result->close();
+
+    $currLat = $row[1];
+    $currLong = $row[2];
+    $currAddr = $row[3];
+
+    return array($currLat, $currLong, $currAddr);
+}
+
+?>
--- model.php
+++ model.php
@@ -1,16 +1,34 @@
 <?php
+
+/*
+//TODO: need google mapping functionality
+function calculateCost(String $start, String $end) {}
+
+//TODO: Need this functionality eventually
+function updateDeliveries() {}
+function requestDelivery(){}
+function acceptDelivery() {}
+function driverDelivered(String $user_id) {}
+*/
+
+
 require_once 'includes/conn.php';
 require_once 'includes/session.php';
 require_once 'includes/auth.php';
+include 'db.php';
 include 'maps.php';
 
 // prevent unauthorized access
 if ($_SESSION['authenticated'] != true || $_SESSION["username"] == NULL)
-    die("Not logged in");
+  die("{'status':'STATUS_ERROR','error':'Not logged in'}");
 
 
-function getUserInformation(String $username)
-{
+// JUST AN EXAMPLE OF GETTING SOMETHING FROM MAPS API - REMOVE LATER
+function getFromMapsApiDemo($friendlyName){
+  return getGeocode($friendlyName);
+}
+
+function getUserInformation(String $username) {
   global $conn;
   $query = "SELECT * FROM user WHERE username = '$username'"; //
   $result = $conn->query($query);
@@ -20,90 +38,45 @@ function getUserInformation(String $username)
   return $info;
 }
 
-function setLocationTransaction(String $username, String $newLat, String $newLong, String $newAddr)
-{
-  global $conn;
-  // notice user has a t_id to show CURRENT location
-  // t_id and loc_id is AUTO-INCREMENT
-  // - this needs to be changed accordingly
-  // INSERT to transactions - a location update is a transaction with TIME_STAMP
-  // INSERT to location ....
-  $user_id = getUserInformation($username)[0];
-
-  // Create a new location
-  $query = "INSERT INTO location(lat, lon, address) VALUES('$newLat', '$newLong', '$newAddr')";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
-
-  $new_loc_id = $conn->insert_id; // the location id is the most recently inserted
+function updateLocation(String $user_id, String $newLat, String $newLong, String $newAddr) {
+  $new_loc_id = insertNewLocationToDb($newLat, $newLong, $newAddr);
   $t_type = "loc update";
-  $start_location = $new_loc_id;
-  // Create a new transaction
-  $query = "INSERT INTO transaction(t_type, restaurant_id, start_loc) VALUES('$t_type', '$user_id', '$start_location')";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
+  $new_t_id = insertNewTransactionToDb($user_id, $new_loc_id, $t_type);
+  updateUserTransactionInDb($user_id, $new_t_id);
 }
 
-function updateDriverLocation(String $username, String $newLat, String $newLong, String $newAddr)
-{
-  global $conn;
-  $t_id = getUserInformation($username)[5];
-  $loc_to_update = getLocationByTid($t_id);
-
-  // Update location
-  $query = "UPDATE location SET lat = '$newLat', lon = '$newLong', address = '$newAddr' WHERE loc_id = '$loc_to_update'";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
+function getCurrentUserLocationByTid($t_id){
+  return getLocationByLocId(getLocationIdByTid($t_id));
 }
 
-function RestaurantUpdateAddress(String $user_id, String $newLat, String $newLong, String $newAddr)
-{
-  global $conn;
-  // Create new location and transaction
-  setLocationTransaction($user_id, $newLat, $newLong, $newAddr);
-  $new_t_id = $conn->insert_id;
+//function updateDriverLocation(String $username, String $newLat, String $newLong, String $newAddr)
+//{
+//  global $conn;
+//  $t_id = getUserInformation($username)[5];
+//  $loc_to_update = getLocationByTid($t_id);
+//
+//  // Update location
+//  $query = "UPDATE location SET lat = '$newLat', lon = '$newLong', address = '$newAddr' WHERE loc_id = '$loc_to_update'";
+//  $result = $conn->query($query);
+//  if(!$result) die($conn->error);
+//}
 
-  // Restaurant's t_id is updated to the new values
-  $query = "UPDATE user SET t_id = '$new_t_id' WHERE user_id = '$user_id'";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
-}
+//function RestaurantUpdateAddress(String $user_id, String $newLat, String $newLong, String $newAddr)
+//{
+//  global $conn;
+//  // Create new location and transaction
+//  setLocationTransaction($user_id, $newLat, $newLong, $newAddr);
+//  $new_t_id = $conn->insert_id;
+//
+//  // Restaurant's t_id is updated to the new values
+//  $query = "UPDATE user SET t_id = '$new_t_id' WHERE user_id = '$user_id'";
+//  $result = $conn->query($query);
+//  if(!$result) die($conn->error);
+//}
 
-// JUST AN EXAMPLE OF GETTING SOMETHING FROM MAPS API - REMOVE LATER
-function getFromMapsApiDemo($friendlyName){
-    return getGeocode($friendlyName);
-}
 
 
-function getLocationByTid($t_id){
-  global $conn;
-  $query = "SELECT start_loc FROM transaction WHERE t_id = '$t_id'";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
 
-  $row = $result->fetch_array(MYSQLI_NUM);
-  $loc = $row[4];
-
-  return $loc;
-}
-
-// helper function
-function getLocationById(String $loc_id)
-{
-  global $conn;
-  $query = "SELECT * FROM location WHERE loc_id = '$loc_id'";
-  $result = $conn->query($query);
-  if(!$result) die($conn->error);
-
-  $row = $result->fetch_array(MYSQLI_NUM);
-  $result->close();
-
-  $currLat = $row[1];
-  $currLong = $row[2];
-  $currAddr = $row[3];
-
-  return array($currLat, $currLong, $currAddr);
-}
 
 function getCurrentDeliveries(String $user_id)
 {
@@ -119,26 +92,4 @@ function getCurrentDeliveries(String $user_id)
   return $deliveryInfo;
 }
 
-function updateDeliveries()
-{
-
-}
-
-//TODO: need google mapping functionality
-function calculateCost(String $start, String $end)
-{
-
-}
-
-//TODO: Need this functionality eventually
-function requestDelivery()
-{
-
-}
-function acceptDelivery() {
-
-}
-function driverDelivered(String $user_id) {
-
-}
 ?>
